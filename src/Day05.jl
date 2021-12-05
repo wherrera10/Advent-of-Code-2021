03e6da30bda0190b99b009baf08c65242a7c9a3b
Bill <wherrera10@users.noreply.github.com>
2021-12-05 13:35:34 -1000

    Update Day05.jl

--- src/Day05.jl
+++ src/Day05.jl
@@ -1,32 +1,19 @@
 # Advent of code 2021, day 5
 
-function parseinput()
-    txtlines = strip.(readlines("AoCdata/AoC_2021_day5.txt"))
-    return [[parse(Int, s) for s in split(line, r"[\D]+")] for line in txtlines if !isempty(line)]
-end
-
-function allpoints(seg, nodiags)
-    x1, y1, x2, y2 = seg
-    if x1 != x2 && y1 != y2
-        nodiags && return Vector{Int}[]
+function multicrossings(fname, countdiagonals)
+    grid = zeros(Int, 1000, 1000)
+    for txtsegment in [split(s, r"[\D]+") for s in strip.(readlines(fname)) if !isempty(s)]
+        x1, y1, x2, y2 = map(s -> parse(Int, s), txtsegment)
         xsign, ysign = sign(x2 - x1), sign(y2 - y1)
-        return [[x1 + xsign * i, y1 + ysign * i] for i in 0:abs(x1 - x2)]
-    elseif x1 != x2
-        return [[x, y1] for x in min(x1, x2):max(x1, x2)]
-    else
-        return [[x1, y] for y in min(y1, y2):max(y1, y2)]
+        if xsign * ysign == 0 || countdiagonals
+            for p in [[x1 + xsign * i, y1 + ysign * i] for i in 0: abs(xsign == 0 ? y1 - y2 : x1 - x2)]
+                grid[first(p), last(p)] += 1
+            end
+        end
     end
+    return count(grid .> 1)
 end
 
-function multicrossings(nodiags = true)
-    pointcounts = Dict{Vector{Int}, Int}()
-    for segment in parseinput(), p in allpoints(segment, nodiags)
-        pointcounts[p] = haskey(pointcounts, p) ? pointcounts[p] + 1 : 1
-    end
-    return count(values(pointcounts) .> 1)
-end
+println("Part 1: ", multicrossings("AoCdata/AoC_2021_day5.txt", false))
+println("Part 2: ", multicrossings("AoCdata/AoC_2021_day5.txt", true))
 
-println("Part 1: ", multicrossings(true))
-println("Part 2: ", multicrossings(false))
-
-        
