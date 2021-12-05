e35adb125d6fe022a754e8440b1ac089ff7d900e
Bill <wherrera10@users.noreply.github.com>
2021-12-04 21:22:09 -1000

    Update Day05.jl

--- src/Day05.jl
+++ src/Day05.jl
@@ -1,7 +1,6 @@
 # Advent of code 2021, day 5
 
 function parseinput()
-    lines = Vector{Int}[]
     txtlines = strip.(readlines("AoCdata/AoC_2021_day5.txt"))
     return [[parse(Int, s) for s in split(line, r"[\D]+")] for line in txtlines if !isempty(line)]
 end
@@ -9,7 +8,7 @@ end
 function allpoints(seg, nodiags)
     x1, y1, x2, y2 = seg
     if x1 != x2 && y1 != y2
-        nodiags && return []
+        nodiags && return Vector{Int}[]
         xsign, ysign = sign(x2 - x1), sign(y2 - y1)
         return [[x1 + xsign * i, y1 + ysign * i] for i in 0:abs(x1 - x2)]
     elseif x1 != x2
@@ -22,16 +21,12 @@ end
 function multicrossings(nodiags = true)
     pointcounts = Dict{Vector{Int}, Int}()
     for segment in parseinput(), p in allpoints(segment, nodiags)
-        if haskey(pointcounts, p)
-            pointcounts[p] = pointcounts[p] + 1
-        else
-            pointcounts[p] = 1
-        end
+        pointcounts[p] = haskey(pointcounts, p) ? pointcounts[p] + 1 : 1
     end
-    multicross = [p[1] for p in pointcounts if p[2] > 1]
-    return length(multicross)
+    return count(p[2] > 1 for p in pointcounts)
 end
 
 println("Part 1: ", multicrossings(true))
 println("Part 2: ", multicrossings(false))
 
+        
