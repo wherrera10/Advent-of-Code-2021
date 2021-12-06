ee5ffd2b8aac408a995605de4a855dbd005353e8
Bill <wherrera10@users.noreply.github.com>
2021-12-05 20:05:45 -1000

    Update Day06.jl

--- src/Day06.jl
+++ src/Day06.jl
@@ -1,1 +1,23 @@
+# Advent of code 2021, day 6
+
+function parseinput(fname = "AoCdata/AoC_2021_day6.txt")
+   input6 = read(fname, String)
+   return [parse(Int8, x) for x in strip.(split(input6, ",")) if !isempty(input6)]
+end
+
+function simulation!(lfvec::Vector{Int8}, days)
+    numeach = zeros(BigInt, 9)
+    for n in lfvec
+        numeach[n + 1] += 1
+    end
+    for i in 1:days
+        numeach = circshift(numeach, -1)
+        numeach[7] += numeach[end]
+        #println("end of day $i: ", length(lfvec))
+    end
+    return sum(numeach)
+end
+
+@show simulation!(parseinput(), 80)
+@show simulation!(parseinput(), 256)
 
