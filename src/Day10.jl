49651e98a8b556c76d89cad374bdf55f309adb40
Bill <wherrera10@users.noreply.github.com>
2021-12-09 23:06:28 -1000

    Update Day10.jl

--- src/Day10.jl
+++ src/Day10.jl
@@ -1,24 +1,29 @@
 function day10()
     part = [0, 0]
     day10lines = filter(!isempty, strip.(readlines("AoCdata/AoC_2021_day10.txt")))
-    leftop, rightop, vals = ['(', '[', '{', '<'], [')', ']', '}', '>'], [3, 57, 1197, 25137]
-    scores = []
+    rightop, vals, scores = [')', ']', '}', '>'], [3, 57, 1197, 25137], Int[]
     for line in day10lines
         corrupted = false
         rights = Char[]
         for c in line
-            if c in leftop
-                push!(rights, rightop[findfirst(x -> x == c, leftop)])
-            elseif c in rightop
+            if c == '('
+                push!(rights, ')'); continue
+            elseif c == '['
+                push!(rights, ']'); continue
+            elseif c == '{'
+                push!(rights, '}'); continue
+            elseif c == '<'
+                push!(rights, '>'); continue
+            else
                 if isempty(rights) || c != rights[end]
-                    part[1] += vals[findfirst(x -> x == c, rightop)]
+                    part[1] += vals[findfirst(==(c), rightop)]
                     corrupted = true
                 else
                     pop!(rights)
                 end
             end
         end
-        !corrupted && push!(scores, reduce((x, y) -> 5x + findfirst(i -> i == y, rightop), reverse(rights), init=0))
+        !corrupted && push!(scores, reduce((x, y) -> 5x + findfirst(==(y), rightop), reverse(rights), init=0))
     end
     part[2] = sort(scores)[(length(scores) + 1) ÷ 2]
     return part
