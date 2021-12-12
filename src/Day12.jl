1deb768bd2afd57f633b130af27efc0ac1a7c15e
Bill <wherrera10@users.noreply.github.com>
2021-12-12 08:23:04 -1000

    Update Day12.jl

--- src/Day12.jl
+++ src/Day12.jl
@@ -25,37 +25,19 @@ QW-dz
 ps-dz
 """
 
-function day12()
-    part = [0, 0]
-    day12lines = filter(!isempty, strip.(split(data12, "\n")))
-    vertices = Dict{String, Vector{String}}()
-    for line in day12lines
-        v1, v2 = split(line, "-")
-        if haskey(vertices, v1)
-            push!(vertices[v1], v2)
-        else
-            vertices[v1] = [v2]
-        end
-        if haskey(vertices, v2)
-            push!(vertices[v2], v1)
-        else
-            vertices[v2] = [v1]
-        end
-    end
-
     """ recursive depth first search """
-    function allpaths(startvertex, currentpath, doublecaveok)
+    function allpaths(startvertex, currentpath, part1 = true)
         startvertex == "end" && return [currentpath]
-        a = Vector{String}[]
+        paths = Vector{String}[]
         for nextvertex in vertices[startvertex]
             nextvertex == "start" && continue
             if isuppercase(first(nextvertex)) || !(nextvertex in currentpath)
-                append!(a, allpaths(nextvertex, vcat(currentpath, [nextvertex]), part1))
+                append!(paths, allpaths(nextvertex, vcat(currentpath, [nextvertex]), part1))
             elseif !part1 && islowercase(first(nextvertex))
-                append!(a, allpaths(nextvertex, vcat(currentpath, [nextvertex])))
+                append!(paths, allpaths(nextvertex, vcat(currentpath, [nextvertex])))
             end
         end
-        return a
+        return paths
     end
 
     part[1] = length(allpaths("start", ["start"], false))
