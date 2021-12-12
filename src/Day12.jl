366c015effa0273843660b013f9a63b811ed90d9
Bill <wherrera10@users.noreply.github.com>
2021-12-12 01:56:58 -1000

    Update Day12.jl

--- src/Day12.jl
+++ src/Day12.jl
@@ -1,1 +1,71 @@
+using Graphs
+
+data12 = """
+hl-WP
+vl-fo
+vl-WW
+WP-start
+vl-QW
+fo-wy
+WW-dz
+dz-hl
+fo-end
+VH-fo
+ps-vl
+FN-dz
+WP-ps
+ps-start
+WW-hl
+end-QW
+start-vl
+WP-fo
+end-FN
+hl-QW
+WP-dz
+QW-fo
+QW-dz
+ps-dz
+"""
+
+function day12()
+    part = [0, 0]
+    day12lines = filter(!isempty, strip.(split(data12, "\n")))
+    vertices = Dict{String, Vector{String}}()
+    for line in day12lines
+        v1, v2 = split(line, "-")
+        if haskey(vertices, v1)
+            push!(vertices[v1], v2)
+        else
+            vertices[v1] = [v2]
+        end
+        if haskey(vertices, v2)
+            push!(vertices[v2], v1)
+        else
+            vertices[v2] = [v1]
+        end
+    end
+
+    function allpaths(startvertex, currentpath, doublecaveok)
+        startvertex == "end" && return [currentpath]
+        a = Vector{String}[]
+        for nextvertex in vertices[startvertex]
+            nextvertex == "start" && continue
+            if isuppercase(first(nextvertex)) || !(nextvertex in currentpath)
+                append!(a, allpaths(nextvertex, vcat(currentpath, [nextvertex]), doublecaveok))
+            elseif doublecaveok && islowercase(first(nextvertex))
+                append!(a, allpaths(nextvertex, vcat(currentpath, [nextvertex]), false))
+            end
+        end
+        return a
+    end
+
+    part[1] = length(allpaths("start", ["start"], false))
+    part[2] = length(allpaths("start", ["start"], true))
+
+    return part
+end
+
+part = day12()
+println("Part 1:", part[1])
+println("Part 2:", part[2])
 
