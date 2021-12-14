5d87ff4b16cfd47dc22c71b25cf0e8a34b609657
Bill <wherrera10@users.noreply.github.com>
2021-12-13 15:40:55 -1000

    Update Day13.jl

--- src/Day13.jl
+++ src/Day13.jl
@@ -1,65 +1,65 @@
-function day13()
-    dottext, foldtext = split(read("AoCdata/AoC_2021_day13.txt", String), "\n\n")
-    dotlines = strip.(split(dottext, "\n"))
-    dots = [split(line, ",") .|> s -> parse(Int, s) for line in dotlines]
-    ncols, nrows = maximum([first(dot) for dot in dots]) + 1, maximum([last(dot) for dot in dots]) + 1
-    mat = zeros(Int, nrows, ncols)
-    for dot in dots
-        mat[dot[2] + 1, dot[1] + 1] = 1
-    end
-    foldlines = strip.(split(foldtext, "\n"))
-    folds = Tuple{Bool,Int}[]
-    for line in filter(!isempty, foldlines)
-        isy, pos = split(line, r"[ =]")[(end - 1):end]
-        push!(folds, (isy == "y", parse(Int, pos)))
+using BenchmarkTools
+
+function day13(printoutput = true)
+    folds = Tuple{Char, Int}[]
+    dots = Set{Tuple{Int, Int}}()
+    for line in filter(!isempty, strip.(readlines("AoCdata/day13.txt")))
+        left, right = split(line, r"[ =,]")[end-1:end]
+        if left in ["x", "y"]
+            push!(folds, (first(left), parse(Int, right)))
+        else
+            push!(dots, (parse(Int, left) + 1, parse(Int, right) + 1))
+        end
     end
 
     for (i, fold) in enumerate(folds)
-        if first(fold)
-            top = mat[begin:last(fold), :]
-            bottom = reverse(mat[(last(fold) + 2):end, :]; dims=1)
-            toprows, bottomrows = size(top, 1), size(bottom, 1)
-            if toprows > bottomrows
-                mat = vcat(
-                    top[begin:(toprows - bottomrows), :],
-                    top[(toprows - bottomrows + 1):end, :] .+ bottom,
-                )
-            elseif toprows == bottomrows
-                mat = top .+ bottom
-            else
-                mat = vcat(
-                    bottom[begin:(bottomrows - toprows), :],
-                    bottom[(bottomrows - toprows + 1):end, :] .+ top,
-                )
+        if first(fold) == 'y'  # fold up
+            for d in dots
+                if last(d) > last(fold)
+                    delete!(dots, d)
+                    push!(dots, (first(d), 2 * last(fold) - last(d) + 2))
+                end
             end
         else
-            left = mat[:, begin:last(fold)]
-            right = reverse(mat[:, (last(fold) + 2):end]; dims=2)
-            leftcols, rightcols = size(left, 2), size(right, 2)
-            if leftcols > rightcols
-                mat = hcat(
-                    left[:, begin:(leftcols - rightcols)],
-                    left[:, (leftcols - rightcols + 1):end] .+ right,
-                )
-            elseif leftcols == rightcols
-                mat = left .+ right
-            else
-                mat = hcat(
-                    right[:, begin:(rightcols - leftcols)],
-                    right[:, (rightcols - leftcols + 1):end] .+ left,
-                )
+            for d in dots
+                if first(d) > last(fold)
+                    delete!(dots, d)
+                    push!(dots, (2 * last(fold) - first(d) + 2, last(d)))
+                end
             end
         end
-        i == 1 && println("Part 1: ", count(mat .> 0))
+        printoutput && i == 1 && println("Part 1: ", length(dots))
+    end
+    mat = fill('.', maximum([first(d) for d in dots]), maximum([last(d) for d in dots]))
+    for d in CartesianIndices(mat)
+        (d[1], d[2]) in dots && (mat[d] = '#')
     end
 
-    println("Part 2 graphic:")
-    for row in eachrow(mat)
-        for n in row
-            print(n == 0 ? ". " : "# ")
+    if printoutput
+        println("Part 2 graphic:")
+        for line in eachcol(mat)
+            for ch in line
+                print("$ch ")
+            end
+            println()
         end
-        println()
     end
+
 end
 
 day13()
+
+@btime day13(false)
+
+        #=
+        Part 1: 724
+Part 2 graphic:
+. # # . . # # # . . . . # # . # # # . . # # # # . # # # . . # . . # . # . . . 
+# . . # . # . . # . . . . # . # . . # . # . . . . # . . # . # . . # . # . . . 
+# . . . . # . . # . . . . # . # # # . . # # # . . # . . # . # . . # . # . . . 
+# . . . . # # # . . . . . # . # . . # . # . . . . # # # . . # . . # . # . . . 
+# . . # . # . . . . # . . # . # . . # . # . . . . # . # . . # . . # . # . . . 
+. # # . . # . . . . . # # . . # # # . . # # # # . # . . # . . # # . . # # # # 
+  1.541 ms (5296 allocations: 583.14 KiB)
+=#
+        
