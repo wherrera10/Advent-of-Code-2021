dbea25a2058fce1e37021d5e522081e41c21288a
Bill <wherrera10@users.noreply.github.com>
2021-12-13 22:39:24 -1000

    Update Day14.jl

--- src/Day14.jl
+++ src/Day14.jl
@@ -1,3 +1,5 @@
+using BenchmarkTools
+
 function day14()
     part = [0, 0]
     lines = strip.(readlines("AoCdata/AoC_2021_day14.txt"))
@@ -6,10 +8,10 @@ function day14()
 
     function steps(N)
         lettercounts = zeros(Int, 26)
-        paircounts = Dict(template[i:i+1] => big"1" for i in 1:length(template)-1)
+        paircounts = Dict(template[i:i+1] => 1 for i in 1:length(template)-1)
         paircounts[template[end:end]] = 1
         for step in 1:N
-            tmpdict = Dict{String, BigInt}()
+            tmpdict = Dict{String, Int}()
             for (k, v) in paircounts
                 if haskey(rules, k)
                     tmpdict[k[begin] * rules[k]] = get!(tmpdict, k[begin] * rules[k], 0) + v
@@ -34,3 +36,11 @@ end
 part = day14()
 println("Part 1: ", part[1])
 println("Part 2: ", part[2])
+
+@btime day14()
+
+#=
+Part 1: 2170
+Part 2: 2422444761283
+  1.191 ms (17812 allocations: 870.38 KiB)
+=#
