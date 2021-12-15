ae7fa5a4b732ee785c26b24edee95e5ac1955ace
Bill <wherrera10@users.noreply.github.com>
2021-12-15 09:00:19 -1000

    Update Day15.jl

--- src/Day15.jl
+++ src/Day15.jl
@@ -1,9 +1,9 @@
 using Graphs
 using SparseArrays
 
-function astardigraph(mat, nrows, ncols, startindex, endindex)
+function dijkstra_digraph(mat, nrows, ncols, startindex, endindex)
     dist = spzeros(Int, length(mat), length(mat))
-    g = DiGraph(length(mat))
+    g = SimpleDiGraph(length(mat))
     for i in 1:length(mat)
         if i % nrows != 0
             add_edge!(g, i, i + 1)
@@ -18,8 +18,7 @@ function astardigraph(mat, nrows, ncols, startindex, endindex)
             dist[i + nrows, i] = mat[i]
         end
     end
-    as1 = a_star(g, startindex, endindex, dist)
-    return [x.dst for x in as1]
+    return dijkstra_shortest_paths(g, startindex, dist).dists[endindex]
 end
 
 function day15()
@@ -30,16 +29,15 @@ function day15()
     for i in 1:nrows
         mat[i, :] = parse.(Int, collect(lines[i]))
     end
-    endpoints = astardigraph(mat, nrows, ncols, 1, length(mat))
-    part[1] = sum(mat[i] for i in endpoints)
+    part[1] = dijkstra_digraph(mat, nrows, ncols, 1, length(mat))
 
     mat2 = zeros(Int, ncols * 5, nrows * 5)
     for c in CartesianIndices(mat2)
         mat2[c] = mod1(mat[mod1(c[1], nrows), mod1(c[2], ncols)] +
            (c[1] - 1) ÷ nrows + (c[2] - 1) ÷ ncols, 9)
     end
-    endpoints2 = astardigraph(mat2, nrows * 5, ncols * 5, 1, length(mat2))
-    part[2] = sum(mat2[i] for i in endpoints2)
+    part[2] = dijkstra_digraph(mat2, nrows * 5, ncols * 5, 1, length(mat2))
+
     return part
 end
 
@@ -47,3 +45,4 @@ part = day15()
 println("Part 1: ", part[1])
 println("Part 2: ", part[2])
 
+@time day15()
