2d3ed498688916a72e09a26468880ae5ab4f849a
Bill <wherrera10@users.noreply.github.com>
2021-12-17 07:16:25 -1000

    Update Day17.jl

--- src/Day17.jl
+++ src/Day17.jl
@@ -3,7 +3,7 @@ using BenchmarkTools
 """target area: x=217..240, y=-126..-69"""
 function day17(tx1 = 217, ty1 = -126, tx2 = 240, ty2 = -69)
     part = [0, 0]
-    maxtuple = Tuple{Int, Int, Int}[]
+    maxtuple, yfail = Tuple{Int, Int, Int}[], min(ty1, ty2)
     for i in isqrt(2tx1):tx2, j in ty1:-ty1-1
         x, y, xvel, yvel, maxy, success = 0, 0, i, j, 0, false
         for t in 1:1000
@@ -13,10 +13,12 @@ function day17(tx1 = 217, ty1 = -126, tx2 = 240, ty2 = -69)
             xvel -= sign(xvel)
             yvel -= 1
             if tx1 <= x <= tx2 && ty1 <= y <= ty2
-                !success && push!(maxtuple, (maxy, i, j))
-                success = true
+                if !success
+                    push!(maxtuple, (maxy, i, j))
+                    success = true
+                end
             end
-            y < min(ty1, ty2) && break
+            y < yfail && break
         end
     end
     part[1] = maximum([first(t) for t in maxtuple])
@@ -29,3 +31,4 @@ println("Part 1: ", part[1])
 println("Part 2: ", part[2])
 
 @btime day17()
+
